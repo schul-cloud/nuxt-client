2d24a40d7c385167a8e385872fdc8a464668500f
Murat <murat.merdoglu@hpi.de>
2021-06-02 09:45:32 +0200

    pagination section

--- src/pages/administration/users/index.vue
+++ src/pages/administration/users/index.vue
@@ -1,86 +1,84 @@
 <template>
 	<v-container class="radius-lg">
-		<v-responsive>
-			<v-row>
-				<v-col>
-					<base-breadcrumb :inputs="breadcrumbs" />
-					<h1 class="mb--md h3">
-						{{ $t("pages.administration.students.index.title") }}
-					</h1>
-					<v-text-field
-						v-model="searchQuery"
-						label=""
-						:prepend-inner-icon="mdiIcons.mdiMagnify"
-					/>
+		<v-row>
+			<v-col>
+				<base-breadcrumb :inputs="breadcrumbs" />
+				<h1 class="mb--md h3">
+					{{ $t("pages.administration.students.index.title") }}
+				</h1>
+				<v-text-field
+					v-model="searchQuery"
+					label=""
+					:prepend-inner-icon="mdiIcons.mdiMagnify"
+				/>
 
-					<!--Row selection section-->
-					<div class="blank-section">
-						<div v-if="tableSelection.length" class="row-selection-info">
-							<div class="d-flex align-items-center content-wrapper"></div>
-							<base-button design="icon" class="close" @click="find">
-								<base-icon icon="close" source="material" />
-							</base-button>
-						</div>
+				<!-- #region Row selection section-->
+				<div class="blank-section">
+					<div v-if="tableSelection.length" class="row-selection-info">
+						<div class="d-flex align-items-center content-wrapper"></div>
+						<base-button design="icon" class="close" @click="find">
+							<base-icon icon="close" source="material" />
+						</base-button>
 					</div>
-					<!--Row selection section-->
+				</div>
+				<!-- #endregion -->
 
-					<!--data-table section-->
-					<v-data-table
-						v-model="selected"
-						fluid
-						:headers="tableColumns"
-						:items-per-page="tableData.limit"
-						:items="users"
-						item-key="_id"
-						show-select
-						class="elevation-1"
-						loading
-					>
-						<template v-slot:item.consentStatus="{ item }">
-							<v-icon v-if="item.consentStatus === 'ok'" class="icon-success">
-								{{ mdiIcons.mdiCheckAll }}
-							</v-icon>
-							<v-icon
-								v-if="item.consentStatus === 'missing'"
-								class="icon-danger"
-							>
-								{{ mdiIcons.mdiClose }}
-							</v-icon>
-							<v-icon
-								v-if="item.consentStatus === 'parentsAgreed'"
-								class="icon-warning"
-							>
-								{{ mdiIcons.mdiCheck }}
-							</v-icon>
-						</template>
-						<template v-slot:item.createdAt="{ data }">
-							<span class="text-content">{{ printDate(data) }}</span>
-						</template>
-						<template v-slot:item._id="{ item }">
-							<v-btn icon :to="`/administration/${userType}/${item._id}/edit`">
-								<v-icon>{{ mdiIcons.mdiPencil }}</v-icon>
-							</v-btn>
-						</template>
-					</v-data-table>
-					<!--data-table section-->
+				<!-- #region data-table section-->
+				<v-data-table
+					v-model="selected"
+					fluid
+					:headers="tableColumns"
+					:items-per-page="tableData.limit"
+					:items="users"
+					item-key="_id"
+					show-select
+					class="elevation-1"
+					loading
+				>
+					<template v-slot:item.consentStatus="{ item }">
+						<v-icon v-if="item.consentStatus === 'ok'" class="icon-success">
+							{{ mdiIcons.mdiCheckAll }}
+						</v-icon>
+						<v-icon v-if="item.consentStatus === 'missing'" class="icon-danger">
+							{{ mdiIcons.mdiClose }}
+						</v-icon>
+						<v-icon
+							v-if="item.consentStatus === 'parentsAgreed'"
+							class="icon-warning"
+						>
+							{{ mdiIcons.mdiCheck }}
+						</v-icon>
+					</template>
+					<template v-slot:item.createdAt="{ data }">
+						<span class="text-content">{{ printDate(data) }}</span>
+					</template>
+					<template v-slot:item._id="{ item }">
+						<v-btn icon :to="`/administration/${userType}/${item._id}/edit`">
+							<v-icon>{{ mdiIcons.mdiPencil }}</v-icon>
+						</v-btn>
+					</template>
+				</v-data-table>
+				<!-- #endregion -->
 
-					<!-- #region SampleRegion -->
+				<!-- #region Paginatiom -->
 
-					<v-row align="center">
-						<v-col class="d-flex" cols="6" sm="6" data-app>
-							<v-select
-								:items="tableData.itemPerPage"
-								label="Select"
-								persistent-hint
-								return-object
-							></v-select>
-						</v-col>
-						<v-col class="d-flex flex-row-reverse" cols="6" sm="6"> </v-col>
-					</v-row>
-					<!-- #endregion -->
-				</v-col>
-			</v-row>
-		</v-responsive>
+				<v-row align="center">
+					<v-col class="d-flex" cols="6" sm="6" data-app>
+						<v-select :items="tableData.perPageOptions"></v-select>
+					</v-col>
+					<v-col class="d-flex flex-row-reverse" cols="6" sm="6">
+						41 to 50 from 1003
+						<v-btn icon>
+							<v-icon>{{ mdiIcons.mdiCodeLessThan }}</v-icon>
+						</v-btn>
+						<v-btn icon>
+							<v-icon>{{ mdiIcons.mdiCodeGreaterThan }}</v-icon>
+						</v-btn>
+					</v-col>
+				</v-row>
+				<!-- #endregion -->
+			</v-col>
+		</v-row>
 		<div style="height: 400px"></div>
 	</v-container>
 </template>
@@ -93,6 +91,8 @@ import {
 	mdiClose,
 	mdiCheckAll,
 	mdiPencil,
+	mdiCodeGreaterThan,
+	mdiCodeLessThan,
 } from "@mdi/js";
 import { printDate } from "@plugins/datetime";
 
@@ -162,6 +162,8 @@ export default {
 				mdiClose,
 				mdiCheckAll,
 				mdiPencil,
+				mdiCodeGreaterThan,
+				mdiCodeLessThan,
 			},
 			printDate,
 			tableData: {
@@ -171,14 +173,29 @@ export default {
 				sortOrder: "asc",
 				total: 0,
 				itemPerPage: ["5", "10", "25", "50", "100"],
+				perPageOptions: [
+					{
+						label: this.$t("components.organisms.Pagination.perPage.5"),
+						value: 5,
+					},
+					{
+						label: this.$t("components.organisms.Pagination.perPage.10"),
+						value: 10,
+					},
+					{
+						label: this.$t("components.organisms.Pagination.perPage.25"),
+						value: 25,
+					},
+					{
+						label: this.$t("components.organisms.Pagination.perPage.50"),
+						value: 50,
+					},
+					{
+						label: this.$t("components.organisms.Pagination.perPage.100"),
+						value: 100,
+					},
+				],
 			},
-			items: [
-				{ state: "Florida", abbr: "FL" },
-				{ state: "Georgia", abbr: "GA" },
-				{ state: "Nebraska", abbr: "NE" },
-				{ state: "California", abbr: "CA" },
-				{ state: "New York", abbr: "NY" },
-			],
 		};
 	},
 
